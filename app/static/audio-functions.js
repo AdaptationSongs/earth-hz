aa3efe2b26db9eeef43a1f10b5c0d85ad607b3cf
Ubuntu <ubuntu@tensorflow.bnyweh5dqozunmahqzstb4r1oa.bx.internal.cloudapp.net>
2020-03-17 04:10:26 +0000

    UI for adding labels to arbitrary segments of any recording

--- app/static/audio-functions.js
+++ app/static/audio-functions.js
@@ -1,3 +1,7 @@
+// Global variables
+var current_offset = 0;
+
+
 function playClip(wav_file, offset) {
     var audio = $('audio').get(0);
     if (audio.canPlayType('audio/ogg')) {
@@ -9,6 +13,7 @@ function playClip(wav_file, offset) {
     audio.play();
 }
 
+
 function playStream(wav_file) {
     var audio = $('audio').get(0);
 // Weird stuff happens when seeking in Ogg files - variable bitrate?
@@ -18,30 +23,41 @@ function playStream(wav_file) {
 //        $('audio').attr('src', '/stream_file/'+wav_file+'.mp3');
 //    }
     $('audio').attr('src', '/stream_file/'+wav_file+'.mp3');
+    $('#current-file').text(wav_file);
+    $('#replay-btn').removeClass('disabled');
+    $('#label-btn').removeClass('disabled').attr('href', '/clip/'+wav_file+'/0');
+    $('#spec-img').removeClass('d-none').attr('src', '/spectrogram/'+wav_file+'-0.png');
+
     audio.load();
     audio.play();
+    $(audio).off('timeupdate.full');
+    $(audio).on('timeupdate.full', function() {
+        var new_offset = Math.floor(audio.currentTime / 5) * 5;
+        if (new_offset != current_offset) {
+            current_offset = new_offset;
+            $('#current-offset').text('+'+current_offset+' sec');
+            $('#replay-btn').click(function() {
+                playSelection(current_offset, 5);
+            });
+            $('#label-btn').attr('href', '/clip/'+wav_file+'/'+current_offset);
+            $('#spec-img').attr('src', '/spectrogram/'+wav_file+'-'+current_offset+'.png');
+        }
+    });
 }
 
 
-function playFile(path) {
-    $('audio').attr('src', path);
-    var audio = $('audio').get(0);
-    audio.load();
-    audio.play();
-}
-
-function playSelection(file_name, start, duration) {
-    $('audio').attr('src', '/wav/'+file_name);                                       
+function playSelection(start, duration) {
     var audio = $('audio').get(0);
     var end_float = parseFloat(start) + parseFloat(duration);
     var end = end_float.toFixed(5);
     audio.pause();
     audio.currentTime = start;
     audio.play();
-    $(audio).on('timeupdate', function() {
+    $(audio).on('timeupdate.clip', function() {
         if (audio.currentTime >= end) {
             audio.pause();
-            $(audio).off('timeupdate');
+            audio.currentTime = end - 0.01;
+            $(audio).off('timeupdate.clip');
         } 
     });
 }
