d45d45fffdd2a2499d05a85f0582d99882a5785b
Damian Christey <damian.christey@gmail.com>
2021-07-21 00:49:39 +0000

    Simplify React component build by including them all in one file and adding them to window namespace

---
+++ app/static/src/components.jsx
@@ -0,0 +1,5 @@
+import {FilterForm} from  "../../ml/static/src/filter-form.jsx";
+import {LabelContainer} from "../../labels/static/src/labels.jsx";
+
+window.FilterForm = FilterForm;
+window.LabelContainer = LabelContainer;
